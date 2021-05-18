d21161085261f5cbb21cb8ef205d587e5039d1b0
Eng Sung <engx03@gmail.com>
2021-05-18 00:19:42 -0400

    transalation from english to khmer

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
-
-const axios = require('axios').default;
-const { v4: uuidv4 } = require('uuid');
-
-var subscriptionKey = process.env.AZURE_SUBSCRIPTION_KEY;
-var endpoint = process.env.AZURE_ENDPOINT;
-var location = process.env.AZURE_LOCATION;
-
-async function translateText(){
-  console.log('sub ley: ', subscriptionKey)
-
-    const response = await axios({
-        baseURL: endpoint,
-        url: '/translate',
-        method: 'POST',
-        headers: {
-            'Ocp-Apim-Subscription-Key': subscriptionKey,
-            'Ocp-Apim-Subscription-Region': location,
-            'Content-type': 'application/json',
-            'X-ClientTraceId': uuidv4().toString()
-        },
-        params: {
-            'api-version': '3.0',
-            'from': 'en',
-            'to': ['de', 'it']
-        },
-        data: [{
-            'text': 'Hello World!'
-        }],
-        responseType: 'json'
-    });
-
-    console.log(response.data.translations[0].text);
-
-    return response.data; 
-}
+const transalator = require('./translate');
 
 module.exports = async function App(context) {
-  let data = await translateText();
-  await context.sendText("hi");
+  if (context.event.isText) {
+    let response = await transalator(context.event.text);
+    await context.sendText(response[0].translations[0].text);
+  }
 };
--- src/translate.js
+++ src/translate.js
@@ -3,27 +3,28 @@ const { v4: uuidv4 } = require('uuid');
 
 var subscriptionKey = process.env.AZURE_SUBSCRIPTION_KEY;
 var endpoint = process.env.AZURE_ENDPOINT;
+var location = process.env.AZURE_LOCATION;
 
-export function translateText(){
-    axios({
+module.exports = async function translateText(userInput){
+    const response = await axios({
         baseURL: endpoint,
         url: '/translate',
         method: 'POST',
         headers: {
             'Ocp-Apim-Subscription-Key': subscriptionKey,
+            'Ocp-Apim-Subscription-Region': location,
             'Content-type': 'application/json',
             'X-ClientTraceId': uuidv4().toString()
         },
         params: {
             'api-version': '3.0',
             'from': 'en',
-            'to': ['kh']
+            'to': ['km']
         },
         data: [{
-            'text': 'Hello World!'
+            'text': userInput
         }],
         responseType: 'json'
-    }).then(function(response){
-        return JSON.stringify(response.data, null, 4);
     });
+    return response.data; 
 }
