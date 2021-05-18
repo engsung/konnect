8598834590e7a0ef38372eab5b7b40d01d2a6437
Eng Sung <engx03@gmail.com>
2021-05-17 23:41:45 -0400

    translation api

--- bottender.config.js
+++ bottender.config.js
@@ -23,13 +23,14 @@ module.exports = {
   initialState: {},
   channels: {
     messenger: {
-      enabled: false,
+      enabled: true,
       path: '/webhooks/messenger',
       pageId: process.env.MESSENGER_PAGE_ID,
       accessToken: process.env.MESSENGER_ACCESS_TOKEN,
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+      fields: ['messages', 'messaging_postbacks'],
     },
     whatsapp: {
       enabled: false,
--- src/translate.js
+++ src/translate.js
@@ -1,41 +1,29 @@
-
 const axios = require('axios').default;
 const { v4: uuidv4 } = require('uuid');
 
 var subscriptionKey = process.env.AZURE_SUBSCRIPTION_KEY;
 var endpoint = process.env.AZURE_ENDPOINT;
-var location = process.env.AZURE_LOCATION;
 
-async function translateText(){
-  console.log('sub ley: ', subscriptionKey)
-
-    const response = await axios({
+export function translateText(){
+    axios({
         baseURL: endpoint,
         url: '/translate',
         method: 'POST',
         headers: {
             'Ocp-Apim-Subscription-Key': subscriptionKey,
-            'Ocp-Apim-Subscription-Region': location,
             'Content-type': 'application/json',
             'X-ClientTraceId': uuidv4().toString()
         },
         params: {
             'api-version': '3.0',
             'from': 'en',
-            'to': ['de', 'it']
+            'to': ['kh']
         },
         data: [{
             'text': 'Hello World!'
         }],
         responseType: 'json'
+    }).then(function(response){
+        return JSON.stringify(response.data, null, 4);
     });
-
-    console.log(response.data.translations[0].text);
-
-    return response.data; 
 }
-
-module.exports = async function App(context) {
-  let data = await translateText();
-  await context.sendText("hi");
-};
