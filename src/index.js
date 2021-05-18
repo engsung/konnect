bfe740cb5ffef4fc51236541eb9399f16303a9f5
Eng Sung <engx03@gmail.com>
2021-05-18 01:54:01 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const transalator = require('./translate');
 
 module.exports = async function App(context) {
-  console.log(context);
   if (context.event.isText) {
     let response = await transalator(context.event.text);
     await context.sendText(response[0].translations[0].text);
